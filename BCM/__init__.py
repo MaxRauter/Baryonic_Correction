72972712327f26e2661b06954ab548b9a552af8e
MaxRauter <maxi.rauter@gmx.net>
2025-05-15 15:03:03 +0200

    Troubleshooting readthedocs

--- BCM/__init__.py
+++ BCM/__init__.py
@@ -2,9 +2,9 @@
 Baryonic Correction Model (BCM)
 """
 
-from .abundance_fractions import *
-from .density_profiles import *
-from .utils import *
-from .simulations import *
+from . import abundance_fractions
+from . import density_profiles
+from . import utils
+from . import simulations
 
 __version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="bcm",
+    name="BCM",
     version="0.1.0",
+    description="Baryonic Correction Model",
+    author="Your Name",
     packages=find_packages(),
+    python_requires=">=3.6",
     install_requires=[
         "numpy",
         "scipy",
         "matplotlib",
         "h5py",
-        "hdf5plugin"
+        "hdf5plugin",
+        "tqdm",
     ],
+    extras_require={
+        "docs": [
+            "sphinx>=4.0.0",
+            "sphinx_rtd_theme",
+            "sphinx-autodoc-typehints",
+        ],
+    },
 )
